ff5e3669b29420ee5c9ee6a749c995ec4a3a6017
jgammon612 <jgammon@redhat.com>
2018-12-11 16:32:15 -0600

    Update resources.py

--- project/api/resources.py
+++ project/api/resources.py
@@ -3,7 +3,7 @@
 from tastypie.resources import ModelResource
 from api.models import Credit
 
-class CreditResource(MOdelResource):
+class CreditResource(ModelResource):
     class Meta:
         queryset = Credit.objects.all()
         resource_name = 'credit'
